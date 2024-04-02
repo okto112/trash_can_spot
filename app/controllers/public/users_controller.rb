a6d0012b8b89281442d9550f1cea5678361aa6f3
okto112 <145315359+okto112@users.noreply.github.com>
2024-04-02 12:51:17 +0000

    ユーザー登録、一覧、編集機能追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,4 +4,37 @@ class Public::UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
   end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to public_users_my_page_path
+    else
+      render :edit
+    end
+  end
+
+  def unsubscribe
+    @user = User.find(current_user.id)
+  end
+
+  def withdraw
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      sign_out(current_user)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :is_active )
+  end
 end
