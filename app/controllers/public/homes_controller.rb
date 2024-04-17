8818700419876960814fa55f21d2c65300e503aa
okto112 <145315359+okto112@users.noreply.github.com>
2024-04-17 13:19:02 +0000

    スタイル修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -6,15 +6,24 @@ class Public::HomesController < ApplicationController
   def main
     if params[:key_word] != nil
       @spots = Spot.where("name LIKE ? OR introduction LIKE ?", "%#{params[:key_word]}%", "%#{params[:key_word]}%")
+      if @spots == []
+        flash.now[:alert] = "該当するスポットは見つかりませんでした。"
+      end
       @key_word = params[:key_word]
       @kinds = Kind.all
-    elsif params[:kind_id] != nil
+      @kind_id = 0
+    elsif params[:kind_id] != nil && params[:kind_id] != "0"
       @spotkinds = SpotKind.where(kind_id: params[:kind_id])
       @spots = Spot.where(id: @spotkinds.pluck(:spot_id))
+      if @spots == []
+        flash.now[:alert] = "該当するスポットは見つかりませんでした。"
+      end
       @kinds = Kind.all
+      @kind_id = params[:kind_id]
     else
       @spots = Spot.all
       @kinds = Kind.all
+      @kind_id = 0
     end
   end
 end
