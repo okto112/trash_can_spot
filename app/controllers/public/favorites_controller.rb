1df22be833511fae1bcd39dc891f6841fe130501
okto112 <145315359+okto112@users.noreply.github.com>
2024-04-20 02:47:59 +0000

    お気に入りボタン作成

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,13 +1,15 @@
-class FavoritesController < ApplicationController
+class Public::FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @favorite = Favorite.new(user_id: current_user.id, spot_id: params[:spot_id])
+    @spot = Spot.find(params[:spot_id])
+    @favorite = Favorite.new(user_id: current_user.id, spot_id: @spot.id)
     @favorite.save
   end
 
   def destroy
-    @favorite = Favorite.find_by(user_id: current_user.id, spot_id: params[:spot_id])
+    @spot = Spot.find(params[:id])
+    @favorite = Favorite.find_by(user_id: current_user.id, spot_id: @spot.id)
     @favorite.destroy
   end
 end
