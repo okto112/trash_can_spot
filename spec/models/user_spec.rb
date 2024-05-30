88a9bcbfe0dc1e98225d44dc2fe5ffeba7d3b6fb
okto112 <145315359+okto112@users.noreply.github.com>
2024-05-30 13:21:20 +0000

    RSpec ゲストユーザーのテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,31 @@
 
 require 'rails_helper'
 
-describe 'userモデルのテスト' do
-  it "有効なユーザー情報の場合は保存されるか" do
-    expect(FactoryBot.build(:user)).to be_valid
+RSpec.describe User, type: :model do
+  describe 'ゲストユーザーの作成テスト' do
+    let(:guest_user) { User.guest }
+
+    it 'self.guestメソッドの動作確認' do
+      expect(guest_user).to be_a(User)
+    end
+
+    it 'ゲストユーザーが存在しなかった場合' do
+      expect{guest_user}.to change(User, :count).by(1)
+    end
+
+    it 'ゲストユーザーが存在していた場合' do
+      expect(guest_user).to eq(guest_user)
+    end
+
+    it 'ゲストユーザーの情報が正しいか' do
+      expect(guest_user.name).to eq('ゲストユーザー')
+      expect(guest_user.email).to eq(User::GUEST_USER_EMAIL)
+    end
+  end
+
+  describe 'userのテスト' do
+    it "正しいユーザー情報の場合は保存されるか" do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
   end
 end
