0dbce2f4b1cf106e9cbcae2e96991f302f363767
okto112 <145315359+okto112@users.noreply.github.com>
2024-06-04 13:01:15 +0000

    RSpec user_controllerのテスト実装

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -5,8 +5,10 @@ require 'rails_helper'
 RSpec.describe Public::UsersController, type: :request do
   describe 'public/users_controllerのテスト' do
     let(:test_user) { FactoryBot.create(:user) }
+    let(:guest_user) { User.guest }
 
     describe "before_actionのテスト" do
+
       context ':authenticate_user!のテスト' do
         it "未サインインの場合" do
           get public_users_my_page_path
@@ -18,6 +20,32 @@ RSpec.describe Public::UsersController, type: :request do
 
           get public_users_my_page_path
           expect(response).to be_successful
+        end
+      end
+
+      context ':edit_guest_userのテスト' do
+        it "ゲストユーザーの情報は編集できないことを確認" do
+          sign_in guest_user
+          get public_users_information_edit_path
+          expect(response).to redirect_to(public_users_my_page_path)
+          expect(flash[:alert]).to eq('ゲストユーザーはユーザー情報を編集できません。')
+        end
+      end
+
+      context ':withdrawal_guest_userのテスト' do
+        it "ゲストユーザーは退会確認画面に遷移できないことを確認" do
+          sign_in guest_user
+          get public_users_unsubscribe_path
+          expect(response).to redirect_to(public_users_my_page_path)
+          expect(flash[:alert]).to eq('ゲストユーザーは退会できません。')
+        end
+
+        it "ゲストユーザーは退会できないことを確認" do
+          sign_in guest_user
+          valid_params = { is_active: false }
+          patch public_users_withdraw_path, params: { user: valid_params }
+          expect(response).to redirect_to(public_users_my_page_path)
+          expect(flash[:alert]).to eq('ゲストユーザーは退会できません。')
         end
       end
     end
@@ -70,23 +98,22 @@ RSpec.describe Public::UsersController, type: :request do
       end
 
       context 'withdrawのテスト' do
-        it '正常なリクエストの場合、ユーザーがゴミ箱スポットから退会し、ルートページにリダイレクトされること' do
+        it 'ユーザーの退会処理' do
           sign_in test_user
-          invalid_params = { name: test_user.name, email: test_user.email, is_active: test_user.is_active }
-          patch public_users_withdraw_path, params: { user: invalid_params }
-          sign_in test_user # サインインし直す
+          valid_params = { is_active: false }
+          patch public_users_withdraw_path, params: { user: valid_params }
           expect(response).to redirect_to(root_path)
           expect(response).to have_http_status(302)
           expect(flash[:notice]).to eq('ゴミ箱スポットから退会しました。')
-          expect(test_user.reload.persisted?).to eq(false)
+          expect(test_user.reload.is_active).to eq(false)
         end
 
         it '無効なリクエストの場合、編集ページが再表示されること' do
           sign_in test_user
-          invalid_params = { name: '' } # 無効なパラメータを作成する必要があります
+          invalid_params = { name: '' }
           patch public_users_withdraw_path, params: { user: invalid_params }
           expect(response).to render_template(:edit)
-          expect(response).to have_http_status(200) # 正常に編集ページが表示されることを確認します
+          expect(response).to have_http_status(200)
         end
       end
     end
