f0b3efefda9aad7b7886cc917138321af0246539
okto112 <145315359+okto112@users.noreply.github.com>
2024-06-10 14:05:59 +0000

    spotコントローラのテストコードを実装

--- spec/controllers/public/spot_controller_spec.rb
+++ spec/controllers/public/spot_controller_spec.rb
@@ -4,9 +4,10 @@ require 'rails_helper'
 
 RSpec.describe Public::SpotsController, type: :request do
   describe 'public/users_controllerのテスト' do
+    let(:user) { FactoryBot.create(:user) }
     let(:test_user) { FactoryBot.create(:user) }
     let(:kind) { FactoryBot.create(:kind) }
-    let(:spot) { FactoryBot.create(:spot, kind_ids: [kind.id]) }
+    let(:spot) { FactoryBot.create(:spot, user_id: user.id, kind_ids: [kind.id]) }
 
     describe "before_actionのテスト" do
       context ':authenticate_user!のテスト' do
@@ -16,7 +17,7 @@ RSpec.describe Public::SpotsController, type: :request do
         end
 
         it "サインインの場合" do
-          sign_in test_user
+          sign_in user
           get public_users_my_page_path
           expect(response).to be_successful
         end
@@ -30,10 +31,10 @@ RSpec.describe Public::SpotsController, type: :request do
 
       context ':check_active_userのテスト' do
         it "ログイン中にis_activeがfalseになるとログアウトするかを確認" do
-          sign_in test_user
-          test_user.is_active = false
+          sign_in user
+          user.is_active = false
           get public_users_my_page_path
-          sign_out test_user
+          sign_out user
           expect(response).to redirect_to(root_path)
           expect(flash[:alert]).to eq('先程のアカウントは管理者より使用できなくなりました。')
         end
@@ -65,16 +66,15 @@ RSpec.describe Public::SpotsController, type: :request do
     describe "各アクションのテスト" do
       context 'indexのテスト' do
         it 'ログインユーザーが作成したスポットのみを表示' do
-          sign_in test_user
-          test_spot = FactoryBot.create(:spot, user_id: test_user.id, kind_ids: [kind.id])
-          get public_spots_path
-          expect(assigns(:spots)).to eq([test_spot])
+          sign_in user
+          get public_spots_path, params: { spot: spot.attributes }
+          expect(assigns(:spots)).to eq([spot])
         end
       end
 
       context 'newのテスト' do
         it 'スポット作成の確認' do
-          sign_in test_user
+          sign_in user
           get new_public_spot_path
           expect(assigns(:spot)).to be_a_new(Spot)
         end
@@ -82,18 +82,43 @@ RSpec.describe Public::SpotsController, type: :request do
 
       context 'createのテスト' do
         it 'スポット作成の確認' do
-          sign_in test_user
-          post public_spots_path, params: { spot: { introduction: "説明文" } }
-          expect(assigns(:spot)).to be_a_new(Spot)
-          expect(flash[:notice]).to eq("「#{assigns(:spot).name}」を登録しました！")
+          sign_in user
+          post public_spots_path, params: { spot: { user_id: user.id, name: spot.name, introduction: spot.introduction, latitude: spot.introduction, longitude: spot.longitude, kind_ids: [kind.id] } }
+          expect(flash[:notice]).to eq("「#{spot.name}」を登録しました！")
           expect(response).to redirect_to(public_spots_path)
         end
 
         it "SpotKindのエラー確認" do
-          sign_in test_user
-          post public_spots_path, params: { spot: { introduction: "説明文" }, checked_kinds: [2] }
+          sign_in user
+          post public_spots_path, params: { spot: { introduction: "説明文" } }
           @spot = assigns(:spot)
           expect(@spot.errors.empty?).to eq(false)
+        end
+      end
+
+      context 'updateのテスト' do
+        it 'スポット情報の更新' do
+          sign_in user
+          patch public_spot_path(spot), params: { spot: { id: spot.id, user_id: user.id, name: '新しいスポット', introduction: spot.introduction, latitude: spot.introduction, longitude: spot.longitude, kind_ids: [kind.id] } }
+          expect(response).to redirect_to(public_spot_path(spot))
+          expect(flash[:notice]).to eq("「新しいスポット」を編集しました！")
+        end
+
+        it 'スポット情報の更新に失敗した場合' do
+          sign_in user
+          patch public_spot_path(spot), params: { spot: { introduction: "" } }
+          expect(response).to render_template(:edit)
+          @spot = assigns(:spot)
+          expect(@spot.errors.empty?).to eq(false)
+        end
+      end
+
+      context 'destroyのテスト' do
+        it 'スポットの削除' do
+          sign_in user
+          delete public_spot_path(spot)
+          expect(response).to redirect_to(public_spots_path)
+          expect(flash[:notice]).to eq("「#{spot.name}」を削除しました。")
         end
       end
     end
--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -4,17 +4,17 @@ require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
   describe 'favoriteテーブルにログインユーザーのuser_idがあるか確認' do
-    let(:test_user) { FactoryBot.create(:user) }
+    let(:user) { FactoryBot.create(:user) }
     let(:kind) { FactoryBot.create(:kind) }
     let(:spot) { FactoryBot.create(:spot, kind_ids: [kind.id]) }
 
     it 'user_idがない場合' do
-      expect(spot.favorites.where(user_id: test_user.id).exists?).to eq(false)
+      expect(spot.favorites.where(user_id: user.id).exists?).to eq(false)
     end
 
     it 'user_idがある場合' do
-      Favorite.create(user_id: test_user.id, spot_id: spot.id)
-      expect(spot.favorites.where(user_id: test_user.id).exists?).to eq(true)
+      Favorite.create(user_id: user.id, spot_id: spot.id)
+      expect(spot.favorites.where(user_id: user.id).exists?).to eq(true)
     end
   end
 end
